e1d1c17829f17d0cba820e5f9a3334048bb973ce
Ahmet Danış <ahmetdanis99@gmail.com>
2024-05-24 21:13:20 +0300

    Update main.js
    bodyparser düzenleme + cors

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 require('dotenv').config();
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.set("view engine", "ejs");
 
 app.listen(process.env.PORT, ()=>{
