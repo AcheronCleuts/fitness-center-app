be4d6617f8eb524e85b84b4eb75c5542acebe0f3
Ahmet Danış <ahmetdanis99@gmail.com>
2024-05-25 13:05:19 +0300

    Login error fix
    code :'ERR_HTTP_INVALID_STATUS_CODE'
    fix

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,57 @@
+// const User = require("../models/user");
+// const jwt = require("jsonwebtoken");
+// require("dotenv").config();
+
+// const register = async (req, res) => {
+//   const { name, email, password } = req.body;
+
+//   try {
+//     const user = await User.findOne({ where: { email } });
+//     if (user) {
+//       return res.status(400).send({ message: "Bu e-posta ile bir kullanıcı kayıtlıdır. Lütfen giriş yapınız" });
+//     }
+
+//     const newUser = await User.create({
+//       name,
+//       email,
+//       password,
+//     });
+//     res.status(201).send({ message: "Kayıt işlemi başarıyla gerçekleştirilmiştir" }, user);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send({ message: "Kayıt işlemi sırasında beklenmedik bir hata oluştur" }, error);
+//   }
+// };
+
+// const login = async (req, res) => {
+//   const { email, password } = req.body;
+
+//   try {
+//     const user = await User.findOne({ where: { email } });
+//     if (!user) {
+//       return res.status(400).send({ message: "Kullanıcı Bulunamadı" });
+//     }
+
+//     if (user.password != password) {
+//       return res.status(400).send({ message: "Şifreniz yanlış tekrar deneyiniz" });
+//     }
+
+//     const token = jwt.sign({ userId: user.id }, process.env.SECRET_KEY, { expiresIn: "1h" });
+//     res
+//       .status(200)
+//       .cookie("token", token, { expires: new Date(Date.now() + 15 * 60 * 1000) })
+//       .send({ message: "Giriş Başarılı" });
+//   } catch (error) {
+//     res.status(500).send({ message: "Giriş başarısız" });
+//   }
+// };
+
+// const logout = async (req, res) => {
+//   res.clearCookie("token").send({ message: "Cookieler silindi" });
+// };
+
+// module.exports = { register, login, logout };
+
 const User = require("../models/user");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
@@ -16,10 +70,10 @@ const register = async (req, res) => {
       email,
       password,
     });
-    res.status(201).send("Kayıt işlemi başarıyla gerçekleştirilmiştir", user);
+    res.status(201).send("Kayıt işlemi başarıyla gerçekleştirilmiştir");
   } catch (error) {
     console.error(error);
-    res.status(500).send("Kayıt işlemi sırasında beklenmedik bir hata oluştur", error);
+    res.status(500).send("Kayıt işlemi sırasında beklenmedik bir hata oluştu");
   }
 };
 
@@ -32,7 +86,7 @@ const login = async (req, res) => {
       return res.status(400).send("Kullanıcı Bulunamadı");
     }
 
-    if (user.password != password) {
+    if (user.password !== password) {
       return res.status(400).send("Şifreniz yanlış tekrar deneyiniz");
     }
 
@@ -42,6 +96,7 @@ const login = async (req, res) => {
       .cookie("token", token, { expires: new Date(Date.now() + 15 * 60 * 1000) })
       .send("Giriş Başarılı");
   } catch (error) {
+    console.error(error);
     res.status(500).send("Giriş başarısız");
   }
 };
