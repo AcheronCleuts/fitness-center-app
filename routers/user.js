41d498c3ed5bcb5d7fd4362e31ae93f7b068aa1b
Acheronnn <ahmetdogukana@gmail.com>
2024-05-26 12:57:53 +0300

    Doggy Push
    sex

--- routers/user.js
+++ routers/user.js
@@ -7,4 +7,19 @@ router.post('/register', register);
 router.post('/login', login);
 router.get('/logout', logout);
 
+router.use("/planlar", (req, res)=>{
+    res.render("plans");
+})
+
+router.use("/giris", (req, res)=>{
+    res.render("login");
+})
+
+router.use("/kayit", (req, res)=>{
+    res.render("register");
+})
+
+router.use("/", (req, res) => {
+    res.render("index");
+});
 module.exports = router;
