97a87c0d3295043d047d14c38ba2008a86aa0b5c
Ahmet Danış <ahmetdanis99@gmail.com>
2024-12-09 19:13:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static("public"));
 
-User.hasMany(Reservation, { foreignKey: 'userId' });
-Reservation.belongsTo(User, { foreignKey: 'userId' });
+User.hasMany(Reservation, { foreignKey: "userId" });
+Reservation.belongsTo(User, { foreignKey: "userId" });
 
 app.use(auth);
 app.use(rezervationsRouter);
@@ -28,13 +28,6 @@ app.use("/", (req, res) => {
   res.render("index");
 });
 
-sequelize
-  .sync()
-  .then(() => {
-    app.listen(3000, () => {
-      console.log("Server is running on port 3000");
-    });
-  })
-  .catch((err) => {
-    console.error("Unable to connect to the database:", err);
-  });
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
